7b5f469ccd640137012f14e303beb0236b82a279
jamshed awan <159632135+jamshedawan@users.noreply.github.com>
2024-02-18 15:28:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-document.getElementById("bmiform").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const gender = document.getElementById("gender").value;
-    const age = parseInt(document.getElementById("age").value);
-    const heightFeet = parseInt(document.getElementById("height-feet").value);
-    const heightInches = parseInt(document.getElementById("height-inches").value);
-    const weight = parseFloat(document.getElementById("weight").value);
-  
-    if (gender && age && heightFeet && heightInches && weight) {
-      const height = ((heightFeet * 12) + heightInches) * 0.0254;
-      const bmi = weight / (height * height);
-      const resultElement = document.getElementById("result");
-  
-      let category = "";
-      if (bmi < 18.5) {
-        category = "Underweight";
-      } else if (bmi >= 18.5 && bmi < 24.9) {
-        category = "Normal Weight";
-      } else if (bmi >= 25 && bmi < 29.9) {
-        category = "Overweight";
-      } else {
-        category = "Obese";
-      }
-  
-      // Display the BMI and category
-      let resultMessage = `Your BMI: ${bmi.toFixed(2)}<br>`;
-      resultMessage += `Category: ${category}`;
-      resultElement.innerHTML = resultMessage;
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    }
+    else if(e.target.innerHTML == 'DEL'){
+        string = string.substring(0, string.length-1);
+        input.value = string;
+    }
+    else {
+      string += e.target.innerHTML;
+      input.value = string;
     }
   });
-  
+});
